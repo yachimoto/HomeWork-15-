564c96666f7e53f321b9f384f3fd0e4a66843a61
yachimoto <yachimoto0607@gmail.com>
2019-07-29 12:51:53 +0800

    Firstcommit

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  15
+//
+//  Created by user on 2019/07/13.
+//  Copyright © 2019 谷地元　星斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+}
+
